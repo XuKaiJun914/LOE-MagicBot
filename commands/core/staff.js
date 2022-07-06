813c66bab80693b41548ab571ffe641acb255220
SmallDevil <a0923865232@gmail.com>
2022-07-06 16:17:37 +0800

    Update staff reply

--- commands/core/staff.js
+++ commands/core/staff.js
@@ -15,18 +15,22 @@ module.exports = {
         switch (args[0]) {
             case "staff":
                 message.guild.members.cache.get(user.id).roles.add(role.staff);
+                message.reply(`<@${user.id}> 已獲得 Staff 身份`)
                 break;
             case "mt":
                 message.guild.members.cache.get(user.id).roles.add(role.staff);
                 message.guild.members.cache.get(user.id).roles.add(role.mt);
+                message.reply(`<@${user.id}> 已獲得 MT 身份`)
                 break;
             case "dcdev":
                 message.guild.members.cache.get(user.id).roles.add(role.staff);
                 message.guild.members.cache.get(user.id).roles.add(role.dcdev);
+                message.reply(`<@${user.id}> 已獲得 DC DEV 身份`)
                 break;
             default:
                 message.reply("請輸入正確的職位名稱 [staff, mt, dcdev]")
                 break;
         }
+        message.delete();
     }
 };
